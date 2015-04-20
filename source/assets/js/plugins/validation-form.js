c2b601e7b4c9d0ff30ecc72c99126b7ae2f533c8
luongtronghien1012 <luongtronghien1012@gmail.com>
2015-04-20 18:12:27 +0700

    erewrewrwerew

--- source/assets/js/plugins/validation-form.js
+++ source/assets/js/plugins/validation-form.js
@@ -14,8 +14,33 @@
 ;(function($, window, undefined) {
   var pluginName = 'validationForm';
   var privateVar = null;
-  var privateMethod = function() {
 
+  var msg = {
+    'required': 'Please input value'
+  };
+
+  var createAlertError = function(that, input, message){
+    input.after('<div class="' + that.options.alertError + '">' + message + '</div>');
+  };
+
+  var removeAlertError = function(that, input){
+    input.next('.' + that.options.alertError).remove();
+  };
+
+  var checkRequired = function(that, input) {
+      var message = input.data('validation-required-message');
+
+    if(!input.val() && !input.next('.' + that.options.alertError).length){
+      if(message){
+        createAlertError(that, input, message);
+      }
+      else{
+        createAlertError(that, input, msg.required);
+      }
+    }
+    else{
+      removeAlertError(that, input);
+    }
   };
 
   function Plugin(element, options) {
@@ -26,7 +51,29 @@
 
   Plugin.prototype = {
     init: function() {
-      console.log(1);
+      var that = this,
+        form = that.element,
+        inputVL =  form.find('input,textarea,select').not("[type=submit]"),
+        required = [];
+
+      form.on('submit.' + pluginName, function(e){
+        e.preventDefault();
+        form.data('isValidated', true);
+        $.each(inputVL, function(){
+          var inputVl = $(this);
+          if(inputVl.data('validation-required')){
+            if(!checkRequired(that, inputVl)) {
+              form.data('isValidated', false);
+            }
+          }
+        });
+
+
+        if(form.data('isValidated')) {
+          return true;
+        }
+        return false;
+      });
     },
     publicMethod: function(params) {
 
@@ -50,11 +97,29 @@
   };
 
   $.fn[pluginName].defaults = {
-    option: 'value'
+    formGroup: 'form-group',
+    alertError: 'alert-error'
   };
 
   $(function() {
-    $('[data-' + pluginName + ']')[pluginName]();
+    $('[data-' + pluginName + ']')[pluginName]({
+      'txtPassword': {
+        'lenght': {
+          valid: 6,
+          mess: '> 6ky tu'
+        },
+        required: {
+          valid: true,
+          mess: 'this field is reuqid'
+        }
+      },
+      'txt-username': {
+        'required': {
+          valid: true,
+          mess: 'user  name is reuqied'
+        }
+      }
+    });
   });
 
 }(jQuery, window));
